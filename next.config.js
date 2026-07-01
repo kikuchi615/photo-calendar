21f9c2ece2a5dd9430b362a365b7ccb47dc4a256
kikuchi615 <csma24039@g.nihon-u.ac.jp>
2026-07-01 10:21:14 +0900

    公開できるようにする

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // 静的サイトとして書き出す設定
+  basePath: '/photo-calendar', // GitHub PagesのURLのズレを直す設定
+  images: {
+    unoptimized: true, // 画像最適化機能をオフ（GitHub Pages用）
+  },
+};
 
-module.exports = nextConfig;
+export default nextConfig;
